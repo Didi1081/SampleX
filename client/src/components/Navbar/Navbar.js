1cd9fbf9bea0bd724f79ba058e56aaf98cbfc6cb
Didi <ddapp108@gmail.com>
2019-01-14 12:58:01 -0600

    add developer branch, create react app, create router and nav bar

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './navbar.css'
+import { Redirect, Link } from "react-router-dom";
+
+function Navbar (props) {
+
+
+
+
+    return (
+    <div className="navBar" >
+    <a href="/login"> 
+     <button >Login</button>
+     </a>
+   
+    </div>)
+
+ 
+}
+
+export default Navbar
