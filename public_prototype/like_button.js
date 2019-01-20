2688fc0f35b610b6e15d6159c46f434091e698eb
Didi <ddapp108@gmail.com>
2019-01-20 03:06:12 -0600

    update react component testing

--- public_prototype/like_button.js
+++ public_prototype/like_button.js
@@ -10,7 +10,7 @@ class LikeButton extends React.Component {
 
   render() {
     if (this.state.liked) {
-      return 'You liked this.';
+      return 'You liked product number ' + this.props.commentID;
     }
 
     return e(
@@ -21,5 +21,13 @@ class LikeButton extends React.Component {
   }
 }
 
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll('.like_button_container')
+  .forEach(domContainer => {
+    // Read the comment ID from a data-* attribute.
+    const commentID = parseInt(domContainer.dataset.commentid, 10);
+    ReactDOM.render(
+      e(LikeButton, { commentID: commentID }),
+      domContainer
+    );
+  });
